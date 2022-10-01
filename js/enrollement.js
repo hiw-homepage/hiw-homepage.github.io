1cebe951d7a5a46d8be7ae38b00689251202c731
HIW <87884294+hiw-homepage@users.noreply.github.com>
2022-10-01 12:34:26 +0900

    Add files via upload

---
+++ js/enrollement.js
@@ -0,0 +1,17 @@
+$('html').ready(function() {
+    if(!localStorage.getItem('efSubmitted')) {
+        $('main form label input').attr({
+            'disabled': 'disabled',
+            'title': 'このフォームはすでに送信されています'
+        });
+        $('main form label input').css('opacity', '.5')
+        $('button[type="submit"]').attr('disabled', 'disabled');
+        $('button[type="submit"]').css({
+            'background-color': '#c0c0c0',
+            'border': '4px solid #c0c0c0',
+            'cursor': 'not-allowed'
+        });
+    } else {
+        $('main form label input[type="submit"]').css('transition', '.3s')
+    }
+});
