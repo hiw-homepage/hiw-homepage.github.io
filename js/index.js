e40e7ab74f7fd3ffedece02ad5a569151851303e
HIW <87884294+hiw-homepage@users.noreply.github.com>
2022-11-14 01:19:32 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $('html').ready(function() {
+    if(window.matchMedia('screen and (min-width: 1000px)').matches) {
         let textHeight = $('#text').height();
         $('#copy #cover img').height(textHeight);
+    }
 });
